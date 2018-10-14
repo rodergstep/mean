15ae4d1fe07f835d4205b2f6d2d109617a17639f
Diadumenos <diadumenos@Diadumenoss-MacBook-Pro.local>
2018-10-14 13:56:38 +0300

    added routes for all pages

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -5,6 +5,11 @@ import { SiteLayoutComponent } from './shared/layouts/site-layout/site-layout.co
 import { LoginPageComponent } from './login-page/login-page.component'
 import { RegisterPageComponent } from './register-page/register-page.component'
 import { AuthGuard } from './shared/classes/auth.guard'
+import { OverviewPageComponent } from './overview-page/overview-page.component'
+import { CategoriesPageComponent } from './categories-page/categories-page.component'
+import { OrderPageComponent } from './order-page/order-page.component'
+import { HistoryPageComponent } from './history-page/history-page.component'
+import { AnalyticsPageComponent } from './analytics-page/analytics-page.component'
 
 const routes: Routes = [
   {
@@ -20,7 +25,13 @@ const routes: Routes = [
     path: '',
     component: SiteLayoutComponent,
     canActivate: [AuthGuard],
-    children: []
+    children: [
+      { path: 'overview', component: OverviewPageComponent },
+      { path: 'analytics', component: AnalyticsPageComponent },
+      { path: 'history', component: HistoryPageComponent },
+      { path: 'order', component: OrderPageComponent },
+      { path: 'categories', component: CategoriesPageComponent }
+    ]
   }
 ]
 
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -10,6 +10,12 @@ import { AuthLayoutComponent } from './shared/layouts/auth-layout/auth-layout.co
 import { SiteLayoutComponent } from './shared/layouts/site-layout/site-layout.component'
 import { RegisterPageComponent } from './register-page/register-page.component'
 import { TokenInterseptor } from './shared/classes/token.interseptor'
+import { OverviewPageComponent } from './overview-page/overview-page.component'
+import { AnalyticsPageComponent } from './analytics-page/analytics-page.component'
+import { HistoryPageComponent } from './history-page/history-page.component'
+import { OrderPageComponent } from './order-page/order-page.component'
+import { CategoriesPageComponent } from './categories-page/categories-page.component';
+import { LoaderComponent } from './shared/components/loader/loader.component'
 
 @NgModule({
   declarations: [
@@ -17,7 +23,13 @@ import { TokenInterseptor } from './shared/classes/token.interseptor'
     LoginPageComponent,
     AuthLayoutComponent,
     SiteLayoutComponent,
-    RegisterPageComponent
+    RegisterPageComponent,
+    OverviewPageComponent,
+    AnalyticsPageComponent,
+    HistoryPageComponent,
+    OrderPageComponent,
+    CategoriesPageComponent,
+    LoaderComponent
   ],
   imports: [
     BrowserModule,
