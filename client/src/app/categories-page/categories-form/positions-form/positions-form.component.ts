2b9ba7ecef0b30bd56ca33853be70ae84281fbb9
Diadumenos <diadumenos@Diadumenoss-MacBook-Pro.local>
2018-10-15 16:22:48 +0300

    added category component

---
+++ client/src/app/categories-page/categories-form/positions-form/positions-form.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit, Input } from '@angular/core'
+
+@Component({
+  selector: 'app-positions-form',
+  templateUrl: './positions-form.component.html',
+  styleUrls: ['./positions-form.component.css']
+})
+export class PositionsFormComponent implements OnInit {
+  @Input('categoryId')
+  categoryId: string
+  constructor() {}
+
+  ngOnInit() {}
+}
